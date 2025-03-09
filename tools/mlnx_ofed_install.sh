a17ca3c6262f18e5d392eecaa6405726e89d9770
Martin Kopecký <kopecky@thekrew.app>
2025-03-09 21:07:08 +0100

    Better driver install

--- tools/mlnx_ofed_install.sh
+++ tools/mlnx_ofed_install.sh
@@ -4,8 +4,9 @@
 dnf install cpan gcc-gfortran tk &&
 # download drivers
 wget https://content.mellanox.com/ofed/MLNX_OFED-24.10-2.1.8.0/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
-tar -xf MLNX &&
-cd MLNX 
-./mlnxofedinstall
+tar -xf MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
+~/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64/mlnxofedinstall
+
+
 
 echo "Done please reboot system to load drivers"
