0a53070f039963e44d5cf2881803eb66e3840465
Martin Kopecký <kopecky@thekrew.app>
2025-03-09 21:11:36 +0100

    Adding autoload and cleaner scripts

--- tools/mlnx_ofed_install.sh
+++ tools/mlnx_ofed_install.sh
@@ -5,8 +5,11 @@ dnf install cpan gcc-gfortran tk &&
 # download drivers
 wget https://content.mellanox.com/ofed/MLNX_OFED-24.10-2.1.8.0/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
 tar -xf MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
+# Install drivers 
 ~/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64/mlnxofedinstall
 
+# Create autoload kernel module
+echo ib_ipoib | tee -a /etc/modules-load.d/ib_ipoib.conf
 
+reboot now
 
-echo "Done please reboot system to load drivers"
