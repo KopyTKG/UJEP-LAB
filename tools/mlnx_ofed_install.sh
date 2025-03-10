b86c90f50cabb9426bca7e3193dcd67234066932
Martin Kopecký <kopecky@thekrew.app>
2025-03-10 10:08:27 +0100

    auto install

--- tools/mlnx_ofed_install.sh
+++ tools/mlnx_ofed_install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# update
+dnf update -y
+
 # download deps
-dnf install cpan gcc-gfortran tk &&
+dnf install -y cpan gcc-gfortran tk &&
+
 # download drivers
 wget https://content.mellanox.com/ofed/MLNX_OFED-24.10-2.1.8.0/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
 tar -xf MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
+
 # Install drivers 
 ~/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64/mlnxofedinstall
 
