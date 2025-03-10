e2c58cd61df9ab518a9913a621d6a5cf3e3904aa
Martin Kopecký <kopecky@thekrew.app>
2025-03-10 10:11:22 +0100

    updating driver script

--- tools/mlnx_ofed_install.sh
+++ tools/mlnx_ofed_install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# update
+# Update the system
 dnf update -y
 
-# download deps
-dnf install -y cpan gcc-gfortran tk &&
+# Install dependencies
+dnf install -y cpan gcc-gfortran tk
 
-# download drivers
-wget https://content.mellanox.com/ofed/MLNX_OFED-24.10-2.1.8.0/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
-tar -xf MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz &&
+# Download drivers
+wget https://content.mellanox.com/ofed/MLNX_OFED-24.10-2.1.8.0/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz
+tar -xf MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz
 
-# Install drivers 
+# Install drivers
 ~/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64/mlnxofedinstall
 
 # Create autoload kernel module
 echo ib_ipoib | tee -a /etc/modules-load.d/ib_ipoib.conf
 
+# Reboot the system
 reboot now
-
