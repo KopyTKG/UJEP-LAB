3b41bb8c9229fc69da4139b6f2a3a216ee120d28
Martin Kopecký <kopecky@thekrew.app>
2025-03-10 10:12:54 +0100

    more updating

--- tools/mlnx_ofed_install.sh
+++ tools/mlnx_ofed_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update the system
-dnf update -y
+yes | dnf update -y
 
 # Install dependencies
-dnf install -y cpan gcc-gfortran tk
+yes | dnf install -y cpan gcc-gfortran tk
 
 # Download drivers
 wget https://content.mellanox.com/ofed/MLNX_OFED-24.10-2.1.8.0/MLNX_OFED_LINUX-24.10-2.1.8.0-rhel9.5-x86_64.tgz
