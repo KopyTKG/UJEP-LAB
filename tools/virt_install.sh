b18de345ddedb63e41e5266244317fc0bfa2eb83
Martin Kopecký <kopecky@thekrew.app>
2025-03-11 18:03:54 +0100

    Update virt_install.sh

--- tools/virt_install.sh
+++ tools/virt_install.sh
@@ -4,8 +4,3 @@
 yes | dnf update -y
 yes | dnf install -y qemu-kvm libvirt virt-install virt-manager virt-viewer libguestfs-tools bridge-utils cockpit cockpit-machines pacemaker pcs corosync fence-agents-all resource-agents
 
-
-# start virt
-systemctl enable --now libvirtd
-systemctl enable --now cockpit.socket
-systemctl enable --now pcsd
