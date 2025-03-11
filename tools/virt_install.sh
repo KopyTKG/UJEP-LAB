cf2a662c02c80470f9ed773a463c9fb68ded5d59
Martin Kopecký <kopecky@thekrew.app>
2025-03-11 17:33:30 +0100

    Update virt_install.sh

--- tools/virt_install.sh
+++ tools/virt_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # installing virtualization
-yes | dnf update
-yes | dnf install qemu-kvm libvirt virt-install virt-manager virt-viewer libguestfs-tools bridge-utils cockpit cockpit-machines pacemaker pcs corosync fence-agents-all resource-agents
+yes | dnf update -y
+yes | dnf install -y qemu-kvm libvirt virt-install virt-manager virt-viewer libguestfs-tools bridge-utils cockpit cockpit-machines pacemaker pcs corosync fence-agents-all resource-agents
 
 
 # start virt
