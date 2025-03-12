272aaa7b76910332917e63fcc05d4034271acfea
Martin Kopecký <kopecky@thekrew.app>
2025-03-12 08:45:30 +0100

    Adding more virt

--- tools/virt_install.sh
+++ tools/virt_install.sh
@@ -2,4 +2,5 @@
 
 # installing virtualization
 yes | dnf update -y
-yes | dnf install -y qemu-kvm libvirt virt-install virt-manager virt-viewer libguestfs-tools cockpit cockpit-machines
+yes | dnf install -y qemu-kvm libvirt virt-install virt-manager virt-viewer libguestfs-tools cockpit cockpit-machines --nobest
+
