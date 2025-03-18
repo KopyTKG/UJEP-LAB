b76c3e8b72ce3e4f1d253486a5d2ce98132dbf52
Martin Kopecký <kopecky@thekrew.app>
2025-03-18 13:37:38 +0100

    adding basic automation for openstack

--- README.md
+++ README.md
@@ -15,46 +15,11 @@ curl -fsSl https://raw.githubusercontent.com/KopyTKG/UJEP-LAB/refs/heads/Live/to
 
 3. Static ip on IBS1 link 
 ```bash
-# change ibs1
 sudo nmtui
 ```
 
 4. Virtualization and clustering 
 ```bash
-# install all deps + packages for virt 
 curl -fsSl https://raw.githubusercontent.com/KopyTKG/UJEP-LAB/refs/heads/Live/tools/virt_install.sh | sudo bash
 ```
 
-set password for `hacluster` user on all nodes (**NEEDS TO BE SAME ON ALL NODES**)
-```bash
-sudo passwd hacluster
-```
-
-set proper hostnames on ibs1 network (**ALL NODES**)
-```bash
-nano /etc/hosts
-```
-
-```bash
-10.0.0.1 nodeA.lab nodeA
-10.0.0.2 nodeB.lab nodeB
-10.0.0.3 nodeC.lab nodeC
-10.0.0.4 nodeD.lab nodeD
-```
-
-
-authenticate nodes on main node (**RUN ON ONLY ONE NODE**)
-```bash
-sudo pcs host auth nodeA.lab nodeB.lab nodeC.lab nodeD.lab -u hacluster -p password
-```
-
-create cluster on main node (**RUN ON ONLY ONE NODE**)
-```bash
-sudo pcs cluster setup cluster_name nodeA.lab nodeB.lab nodeC.lab nodeD.lab -u hacluster -p password
-```
-
-start cluster
-```bash
-sudo pcs cluster start --all
-sudo pcs cluster enable --all
-```
--- tools/virt_install.sh
+++ tools/virt_install.sh
@@ -2,5 +2,47 @@
 
 # installing virtualization
 yes | dnf update -y
-yes | dnf install -y qemu-kvm libvirt virt-install virt-manager virt-viewer libguestfs-tools cockpit cockpit-machines --nobest
+yes | dnf install -y qemu-kvm qemu-img libvirt libvirt-client libguestfs-tools python3-libvirt
 
+
+# prep for openstack
+yes | dnf install -y chrony
+
+systemctl enable chronyd.service --now
+
+tee /etc/chrony.conf > /dev/null <<EOF
+server tik.cesnet.cz iburst
+server tak.cesnet.cz iburst
+
+sourcedir /run/chrony-dhcp
+driftfile /var/lib/chrony/drift
+makestep 1.0 3
+rtcsync
+keyfile /etc/chrony.keys
+ntsdumpdir /var/lib/chrony
+leapsectz right/UTC
+logdir /var/log/chrony
+EOF
+
+firewall-cmd --permanent --add-service=ntp
+firewall-cmd --reload
+
+systemctl restart chronyd.service
+
+yes | dnf install dnf-plugins-core -y
+yes | dnf config-manager --set-enabled crb -y
+yes | dnf install centos-release-openstack-dalmatian -y
+yes | dnf upgrade -y
+yes | dnf install python3-openstackclient -y
+yes | dnf install openstack-selinux -y
+
+yes | dnf install mariadb mariadb-server python3-PyMySQL -y
+yes | dnf install rabbitmq-server -y
+
+systemctl enable mariadb.service rabbitmq-server.service --now
+
+echo "Please create or edit /etc/my.cnf.d/openstack.cnf and reload database service"
+echo "https://docs.openstack.org/install-guide/environment-sql-database-rdo.html"
+
+echo "Please follow Rabbitmq confing"
+echo "https://docs.openstack.org/install-guide/environment-messaging-rdo.html"
