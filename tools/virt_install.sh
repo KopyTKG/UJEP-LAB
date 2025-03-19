be44db243e76165976f622de0bfbdff7c45a32af
Martin Kopecký <kopecky@thekrew.app>
2025-03-19 14:34:07 +0100

    Update virt_install.sh

--- tools/virt_install.sh
+++ tools/virt_install.sh
@@ -35,14 +35,3 @@ yes | dnf install centos-release-openstack-dalmatian -y
 yes | dnf upgrade -y
 yes | dnf install python3-openstackclient -y
 yes | dnf install openstack-selinux -y
-
-yes | dnf install mariadb mariadb-server python3-PyMySQL -y
-yes | dnf install rabbitmq-server -y
-
-systemctl enable mariadb.service rabbitmq-server.service --now
-
-echo "Please create or edit /etc/my.cnf.d/openstack.cnf and reload database service"
-echo "https://docs.openstack.org/install-guide/environment-sql-database-rdo.html"
-
-echo "Please follow Rabbitmq confing"
-echo "https://docs.openstack.org/install-guide/environment-messaging-rdo.html"
