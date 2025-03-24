772229ad0d8c720fbe18637e9c6984b23e4654f0
Martin Kopecký <kopecky@thekrew.app>
2025-03-24 18:12:44 +0100

    more tweaks

--- tweaks/nova.perms.sh
+++ tweaks/nova.perms.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
+mkdir /usr/lib/python3.9/site-packages/instances
 chown nova:nova /usr/lib/python3.9/site-packages/
 chmod 775 /usr/lib/python3.9/site-packages/
-touch /usr/lib/python3.9/site-packages/instances
 systemctl start openstack-nova-compute
