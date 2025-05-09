85da06e4ac8a13e1ac7debfd55c59552e5b33f67
Martin Kopecký <kopecky@thekrew.app>
2025-05-09 14:02:07 +0200

    adding client

---
+++ tools/nebula-c.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat << "EOT" > /etc/yum.repos.d/opennebula.repo
+[opennebula]
+name=OpenNebula Community Edition
+baseurl=https://downloads.opennebula.io/repo/6.10/RedHat/$releasever/$basearch
+enabled=1
+gpgkey=https://downloads.opennebula.io/repo/repo2.key
+gpgcheck=1
+repo_gpgcheck=1
+EOT
+yes | yum makecache -y
+
+yes | yum -y install opennebula-node-kvm
+systemctl restart libvirtd
