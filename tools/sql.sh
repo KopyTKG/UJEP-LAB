349f3611d5a96e73846eeee2400c385d8dd0fa89
Martin Kopecký <kopecky@thekrew.app>
2025-05-09 13:08:07 +0200

    rm update

--- tools/sql.sh
+++ tools/sql.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Update the system
-yes | dnf update -y
-if [ $? -ne 0 ]; then
-  echo "Failed to update the system."
-  exit 1
-fi
-
 yes | dnf install -y mariadb-server
 if [ $? -ne 0 ]; then
   echo "Failed to install mariadb-server."
