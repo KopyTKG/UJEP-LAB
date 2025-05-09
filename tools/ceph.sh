22d7d12d7c3b59c698c484c700b084c7bb3a5fde
Martin Kopecký <kopecky@thekrew.app>
2025-05-09 13:17:47 +0200

    ceph

--- tools/ceph.sh
+++ tools/ceph.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-tee /etc/yum.repos.d/ceph.repo > /dev/null <<EOF
-[ceph]
-name=Ceph packages for \$basearch
-baseurl=https://download.ceph.com/rpm-reef/el9/\$basearch
-enabled=1
-priority=2
-gpgcheck=1
-gpgkey=https://download.ceph.com/keys/release.asc
 
-[ceph-noarch]
-name=Ceph noarch packages
-baseurl=https://download.ceph.com/rpm-reef/el9/noarch
-enabled=1
-priority=2
-gpgcheck=1
-gpgkey=https://download.ceph.com/keys/release.asc
-
-[ceph-source]
-name=Ceph source packages
-baseurl=https://download.ceph.com/rpm-reef/el9/SRPMS
-enabled=0
-priority=2
-gpgcheck=1
-gpgkey=https://download.ceph.com/keys/release.asc
-EOF
+yes | dnf install -y centos-release-ceph-reef
 
 yes | dnf install -y cephadm
 
