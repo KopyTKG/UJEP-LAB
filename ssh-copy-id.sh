2a579fb099482df1cbffde7ec4c4838f3c6c0de4
Martin Kopecký <kopecky@thekrew.app>
2026-01-05 21:18:02 +0100

    adding glusterFS with IPoIB only

---
+++ ssh-copy-id.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the number of nodes (from 1 up to this number)
+NODES_COUNT=8
+
+# Define the hostname prefix
+HOST_PREFIX="192.168.1.10"
+SSH_KEY_PATH="/home/kopy/.ssh/ujep_lab.pub"
+
+for I in $(seq 1 $NODES_COUNT); do
+    NODE="${HOST_PREFIX}${I}"
+    ssh-copy-id -i "$SSH_KEY_PATH" "user@$NODE"
+    echo "Copied SSH key to $NODE"
+done
+
